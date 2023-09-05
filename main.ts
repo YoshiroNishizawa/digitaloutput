fd2df91d6936fedd54e651fd948266768cc818a4
YoshiroNishizawa <113979324+YoshiroNishizawa@users.noreply.github.com>
2023-09-05 11:39:53 +0900

    MakeCodeプロジェクトの初期ファイル

---
+++ main.ts
@@ -0,0 +1,7 @@
+input.onButtonPressed(Button.A, function () {
+    pins.digitalWritePin(DigitalPin.P0, 1)
+})
+input.onButtonPressed(Button.B, function () {
+    pins.digitalWritePin(DigitalPin.P0, 0)
+})
+basic.showIcon(IconNames.Pitchfork)
